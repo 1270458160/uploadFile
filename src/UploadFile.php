a99d7ae07ac2ec3817b3f424bc8221afe63aa88a
陈伟武 <chenweiwu@liankaa.com>
2021-07-09 14:27:38 +0800

    测试文件

---
+++ src/UploadFile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Each engineer has a duty to keep the code elegant
+ * Created by chenweiwu at 2021/7/9 13:56
+ */
+
+namespace Ww;
+
+
+class UploadFile
+{
+    public function upload()
+    {
+        echo '上传了一个文件';
+    }
+}
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Each engineer has a duty to keep the code elegant
+ * Created by chenweiwu at 2021/7/9 14:07
+ */
+
+require_once './vendor/autoload.php';
+
+$uploadFile = new \Ww\UploadFile();
+
+$uploadFile->upload();
